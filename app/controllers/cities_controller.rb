31e11cab18a284a46c69f34d726fd6a5303a3b81
Tom Haywood <tom.a.haywood@gmail.com>
2020-03-04 15:30:58 +1100

    added controller actions for cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,18 @@
 class CitiesController < ApplicationController
-   def index
+
+  def index
+    @cities = City.all
+    @countries = Country.all
   end
+
+  def show
+    @city = City.find(params[:id])
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name, :location)
+  end
+
 end
