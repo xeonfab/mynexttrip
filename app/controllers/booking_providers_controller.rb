c3f526f2e36f4921e27e8b2d79d69edf8f074849
Oliver Ayton <ollie@densworth.co.uk>
2020-03-06 12:58:47 +1100

    Tweaks

--- app/controllers/booking_providers_controller.rb
+++ app/controllers/booking_providers_controller.rb
@@ -1,6 +1,15 @@
 class BookingProvidersController < ApplicationController
   def index
     @bookingproviders = BookingProvider.all
+    # @country = Country.find(params[:country_id])
+    # @city = City.find(params[:city_id]) if !params[:city_id].nil?
+
+    if !params[:city_id].nil?
+      @booking_place = City.find(params[:city_id])
+    else
+      @booking_place = Country.find(params[:country_id])
+    end
+
   end
 
   def show
