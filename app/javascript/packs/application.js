1e4f3b26797989a10c5964d98f547715b5af70b8
Fabien Riou <fabien.riou92@gmail.com>
2020-03-06 16:41:34 +1100

    bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 import { initBanner } from '../plugins/init_banner';
 
-// initMapbox();
-
 initBanner();
+initMapbox();
+
+
