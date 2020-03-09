13e5fbd97203c7f94d148b2b1cf22f491fec7366
Fabien Riou <fabien.riou92@gmail.com>
2020-03-09 18:14:33 +1100

    seednew

--- db/climate_api.rb
+++ db/climate_api.rb
@@ -4,33 +4,6 @@ require 'csv'
 
 City.all.each do |city|
 
-  unless city.name.include?('-') || city.name.include?('Area')
-
-    p url_weather = "https://api.openweathermap.org/data/2.5/weather?q=#{ city.name.include?(',') ? city.name.downcase.gsub(' ', "%") : city.name.downcase.gsub(' ', "%20")}&appid=ec7e53d090ba26a48cffe7b676e88bc5"
-    weather_serialized = open(url_weather).read
-    climate_weather = JSON.parse(weather_serialized)
-
-  # Puts "API url ok"
-
-  #   Climate.create!(
-  #     january: climate_weather["main"]["temp"],
-  #     february: climate_weather["main"]["temp"],
-  #     march: climate_weather["main"]["temp"],
-  #     april: climate_weather["main"]["temp"],
-  #     may: climate_weather["main"]["temp"],
-  #     june: climate_weather["main"]["temp"],
-  #     july: climate_weather["main"]["temp"],
-  #     august: climate_weather["main"]["temp"],
-  #     september: climate_weather["main"]["temp"],
-  #     october: climate_weather["main"]["temp"],
-  #     november: climate_weather["main"]["temp"],
-  #     december: climate_weather["main"]["temp"],
-  #     city: city
-  #   )
-  #   Puts "#{city} weather climate created"
-  # end
-
-
   csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
   filepath = File.join(__dir__, 'data/weather.csv')
 
@@ -41,5 +14,16 @@ City.all.each do |city|
 
     # ...
   end
+
+  unless city.name.include?('-') || city.name.include?('Area')
+    p url_weather = "https://api.openweathermap.org/data/2.5/weather?q=#{ city.name.include?(',') ? city.name.downcase.gsub(' ', "%") : city.name.downcase.gsub(' ', "%20")}&appid=ec7e53d090ba26a48cffe7b676e88bc5"
+    weather_serialized = open(url_weather).read
+    climate_weather = JSON.parse(weather_serialized)
+
+
+
+
+
+
 end
 end
