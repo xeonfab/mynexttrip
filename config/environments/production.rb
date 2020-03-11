a5f7abc6cdc5228c9af9813b9144fea6ce6411a7
Fabien Riou <fabien.riou92@gmail.com>
2020-03-12 10:51:49 +1100

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # Random comment
-  # config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # config.assets.css_compressor = :sass
 
